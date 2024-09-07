98259cd8879a2cfbe2659a312560d5707abc3c7a
Sorokovsky <Sorokovskys@ukr.net>
2024-09-07 13:19:25 +0300

    car

--- Car.cs
+++ Car.cs
@@ -1,6 +1,45 @@
 ﻿namespace Task
 {
-    internal class Car
+    internal class Car : Vehicle
     {
+        private int _fuelMax = 0;
+
+        public string Mark {  get; set; } = string.Empty;
+        public int FuelMax
+        {
+            get
+            {
+                return _fuelMax;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    _fuelMax = 0;
+                }
+                else
+                {
+                    _fuelMax = value;
+                }
+            }
+        }
+
+        public Car(string name, int speed, string mark, int fuelMax) : base(name, speed)
+        {
+            Mark = mark;
+            FuelMax = fuelMax;
+        }
+
+        public Car() : base()
+        {
+            
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine("Марка машини: {0}.", Mark);
+            Console.WriteLine("Максимальна кількість палива (л.): {0}.", FuelMax);
+        }
     }
 }
--- Vehicle.cs
+++ Vehicle.cs
@@ -2,6 +2,43 @@
 {
     internal class Vehicle
     {
+        private int _speed = 0;
 
+        public string Name { get; set; } = string.Empty;
+        public int Speed 
+        {
+            get
+            {
+                return _speed;
+            } 
+            set
+            {
+                if(value < 0)
+                {
+                    _speed = 0;
+                }
+                else
+                {
+                    _speed = value;
+                }
+            }
+        }
+
+        public Vehicle()
+        {
+            
+        }
+
+        public Vehicle(string name, int speed)
+        {
+            Speed = speed;
+            Name = name;
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Ім'я: {0}.", Name);
+            Console.WriteLine("Швидкість: {0}.", Speed);
+        }
     }
 }
