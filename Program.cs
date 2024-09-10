67d03e2520a6fa893820167b1ad1da3adc8ea95b
Андрій Сороковський <andrijsorokovskij@MacBook-Air-Andrij.local>
2024-09-10 15:33:23 +0300

    rewrite

--- Program.cs
+++ Program.cs
@@ -4,24 +4,64 @@ namespace Task;
 
 public static class Program
 {
+    private static Repository<Vehicle> vehicles = new();
+    private static Repository<Vehicle> cars = new();
+    private static Repository<Vehicle> trains = new();
+    private static Repository<Vehicle> express = new();
+
     public static void Main()
     {
-        Console.WriteLine("Введення транспортного засобу.");
-        Vehicle vehicle = EnterVehicle();
-        Console.WriteLine("Введення автомобілю.");
-        Car car = EnterCar();
-        Console.WriteLine("Введення поїзда.");
-        Train train = EnterTrain();
-        Console.WriteLine("Введення експресу.");
-        Express express = EnterExpress();
-        Console.WriteLine("Транспортний засіб.");
-        vehicle.Show();
-        Console.WriteLine("Автомобіль.");
-        car.Show();
-        Console.WriteLine("Поїзд.");
-        train.Show();
-        Console.WriteLine("Експрес.");
-        express.Show();
+        while(true) 
+        {
+            Repository<Vehicle> currentRepository = ChooseVehicleType();
+            if(currentRepository == null) {
+                continue;
+            }
+            int operation = ChooseOperation();
+            switch(operation)
+            {
+                case 0:
+                {
+                    return;
+                }
+                case 1: {
+                    if(currentRepository == vehicles) currentRepository.Append(EnterVehicle());
+                    else if(currentRepository == cars) currentRepository.Append(EnterCar());
+                    else if(currentRepository == trains) currentRepository.Append(EnterTrain());
+                    else if(currentRepository == express) currentRepository.Append(EnterExpress());
+                    else currentRepository.Append(EnterVehicle());
+                    break; 
+                }
+                case 2: {
+                    Console.WriteLine("Введіть імя для видалення: ");
+                    string name = Console.ReadLine();
+                    currentRepository.DeleteByName(name);
+                    break;
+                }
+                case 3: {
+                    Console.WriteLine("Введіть імя для пошуку: ");
+                    string name = Console.ReadLine();
+                    var items = currentRepository.GetByName(name);
+                    for(int i = 0; i < items.Count(); i++) {
+                        Console.WriteLine("#{0}", i + 1);
+                        items.ElementAt(i).Show();
+                    }
+                    break;
+                }
+                case 4: {
+                    var items = currentRepository.GetAll();
+                    for(int i = 0; i < items.Count(); i++) {
+                        Console.WriteLine("#{0}", i + 1);
+                        items.ElementAt(i).Show();
+                    }
+                    break;
+                }
+                default: {
+                    Console.WriteLine("Непередбачуваний вибір.");
+                    break;
+                }
+            }
+        }
     }
 
     public static Vehicle EnterVehicle()
@@ -46,7 +86,7 @@ public static class Program
     public static Train EnterTrain()
     {
         int carriageCount;
-        Console.WriteLine("Введіть кількість: "); carriageCount = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введіть кількість вагонів: "); carriageCount = Convert.ToInt32(Console.ReadLine());
         Vehicle vehicle = EnterVehicle();
         return new Train(vehicle, carriageCount);
     }
@@ -57,5 +97,51 @@ public static class Program
         Console.WriteLine("Введіть переваги: "); benefits = Console.ReadLine();
         Train train = EnterTrain();
         return new Express(train, benefits);
+    }
+
+    public static int ChooseOperation() 
+    {
+        Console.WriteLine("Оберіть операцію.");
+        Console.WriteLine("0-Вийти.");
+        Console.WriteLine("1-Додати.");
+        Console.WriteLine("2-Видалити.");
+        Console.WriteLine("3-Знайти за іменем.");
+        Console.WriteLine("4-Показати все.");
+        Console.Write(">> ");
+        return Convert.ToInt32(Console.ReadLine());   
+    }
+
+    public static Repository<Vehicle> ChooseVehicleType()
+    {
+        Console.WriteLine("Виберіть з чим хочете працювати: ");
+        Console.WriteLine("1-Транспортний засіб.");
+        Console.WriteLine("2-Автомобіль.");
+        Console.WriteLine("3-Поїзд.");
+        Console.WriteLine("4-Експрес.");
+        int repoNumber = Convert.ToInt32(Console.ReadLine());
+        switch(repoNumber)
+            {
+                case 1:
+                {
+                    return vehicles;
+                }
+                case 2:
+                {
+                    return cars;
+                }
+                case 3:
+                {
+                    return trains;
+                }
+                case 4:
+                {
+                    return express;
+                }
+                default:
+                {
+                    Console.WriteLine("Непередбачуваний вибір. Повторіть");
+                    return null;
+                }
+            }
     }
 }
---
+++ Repository.cs
@@ -0,0 +1,30 @@
+namespace Task
+{
+    public class Repository<T> where T : Vehicle
+    {
+        private LinkedList<T> _list = new();
+
+        public void Append(T item) 
+        {
+            _list.Append(item);
+        }
+
+        public IEnumerable<T> GetByName(string name)
+        {
+            return _list.Where(x => x.Name.Equals(name));
+        }
+
+        public void DeleteByName(string name)
+        {
+            var selected = GetByName(name);
+            foreach(T item in selected)
+            {
+                _list.Remove(item);
+            }
+        }
+
+        public IEnumerable<T> GetAll() {
+            return _list.Where(x => true);
+        }
+    }
+}
